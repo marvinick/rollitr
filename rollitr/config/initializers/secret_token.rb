9146d42aa744a391c24c40363e4204185bdf5db8
marvinick <marviluw@gmail.com>
2014-06-01 20:25:35 -0500

    initial roll

---
+++ rollitr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rollitr::Application.config.secret_key_base = 'a56e3552467aa23c357fd57d88e716a5fd55b13cbb0a0e0674325bf73ae4941204e12dd76831ab30776d7bbeb8cb4aa8120ae4a439879beeb6316edb8f0c6be4'

3e109a3dcd59f56011cccffe5c3d4a053caab62c
marvinick <marviluw@gmail.com>
2014-06-03 18:11:41 -0500

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rollitr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -27,7 +27,7 @@ Rollitr::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true

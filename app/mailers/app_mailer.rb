2573ecb9f506cb82bf9464f41baf3b044b21a00f
marvinick <marviluw@gmail.com>
2014-06-23 16:02:54 -0500

    chefleo

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,6 +1,6 @@
 class AppMailer < ActionMailer::Base
   def signup_confirmation(user)
     @user = user
-    mail to: user.email, from: "marvin8214@gmail.com", subject: "Get ready for some great food!"
+    mail to: user.email, from: "chefleo@ontherolltulsa.com", subject: "Get ready for some great food!"
   end
 end
